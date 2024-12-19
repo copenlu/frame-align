f98f9e8d7dc52c669a73d4756bfb0ba4197d866f
vsl333 <vsl333@hendrixgate01fl.unicph.domain>
2024-12-19 20:51:38 +0100

    shell script for running model

--- run_vllm.sh
+++ run_vllm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=v_frame   # Job name
+#SBATCH --job-name=medium_f   # Job name
 #SBATCH --array=0
-#SBATCH --gres=gpu:a40:1  # Number of GPUs (per node)
+#SBATCH --gres=gpu:a40:1 
 #SBATCH --cpus-per-task=4     # 8 CPUs per task
 #SBATCH --mem=40GB            # 30GB of memory
-#SBATCH --time=02:00:00        # runtime
+#SBATCH --time=010:00:00        # runtime
 #SBATCH --partition=gpu
 
 # base_dir="/projects/frame_align/data/raw/2023-2024"
@@ -15,6 +15,7 @@
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 # echo "processing file: ${month_dir}/datawithtopiclabels.csv"
 
+# --exclude hendrixgpu03fl,hendrixgpu04fl,hendrixgpu15fl,hendrixgpu21fl,hendrixgpu22fl
 # python src/models/vision/vision_framing_local.py.py --data_file "${month_dir}/datawithtopiclabels.csv" --output_dir "/projects/frame_align/data/srishti_part1/" --model_code mistralai/Mistral-7B-Instruct-v0.3
 # python src/models/vision/vision_framing_local.py --data_file "data_pixtral_llava/sample_data.csv" --dir_name "data_pixtral_llava/output" --model_name llava-hf/llava-1.5-13b-hf
 python src/models/vision/vision_framing_local.py --data_file "data_pixtral_llava/sampled_annotated_articles_600.csv" --dir_name "data_pixtral_llava/output" --model_name "mistralai/Pixtral-12B-2409"
